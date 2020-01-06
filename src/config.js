8158a36b618af3651c502eb36817dc576693b5c7
Dante Calderon <mail@dantecalderon.dev>
2020-01-05 19:16:06 -0500

    Fix env

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const config = {
-  LAMBDA_ENDPOINT: process.env.LAMBDA_ENDPOINT
+  LAMBDA_ENDPOINT: process.env.LAMBDA_ENDPOINT || 'https://url-shortener-app.netlify.com/.netlify/functions'
 }
